90b8b43ac231fbf1d40b7a00f0dfef777b3c49cc
Morteza Sabihi <mortezasabihi87@gmail.com>
2019-12-05 12:12:05 +0330

    navigation component designed

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <navigation />
+  </div>
 </template>
+
+<script>
+import Navigation from "@/components/layout/Navigation";
+
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
