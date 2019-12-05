2578e53b8c9d60844f61fad69c43b382ebb5e988
Morteza Sabihi <mortezasabihi87@gmail.com>
2019-12-05 12:35:27 +0330

    register page designed

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -18,7 +18,9 @@
               <em>User</em>
             </template>
             <b-dropdown-item :to="{ name: 'login' }">Login</b-dropdown-item>
-            <b-dropdown-item href="#">Sign up</b-dropdown-item>
+            <b-dropdown-item :to="{ name: 'register' }"
+              >Sign up</b-dropdown-item
+            >
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,5 +4,11 @@ export default [
     path: "/login",
     component: () =>
       import(/* webpackChunkName: "about" */ "@/views/auth/login")
+  },
+  {
+    name: "register",
+    path: "/register",
+    component: () =>
+      import(/* webpackChunkName: "register" */ "@/views/auth/register")
   }
 ];
